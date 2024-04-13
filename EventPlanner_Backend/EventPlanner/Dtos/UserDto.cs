c9f5e789009da2b49ec3d95d6babea94b8a7a79a
shpend6 <shpend.ismaili1@gmail.com>
2024-04-13 16:24:08 -0400

    Deleted files

--- EventPlanner_Backend/EventPlanner/Dtos/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EventPlanner.Dtos;
-
-public class UserDto
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Event_Planner_LIFE
