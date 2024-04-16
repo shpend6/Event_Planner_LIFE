5e79547e23db69c5847be8e4b134520960769366
shpend6 <shpend.ismaili1@gmail.com>
2024-04-16 00:24:58 -0400

    merged branch

---
+++ EventPlanner/src/hooks/useUsers.ts
@@ -0,0 +1,18 @@
+import useSWR from "swr";
+
+type Users = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  passwordHash: string;
+  createdEvents: unknown; // You can define the type for these properties as well if needed
+  eventsAttending: unknown;
+}[];
+
+export const useUser = () => {
+  const { data, isLoading, error } = useSWR<Users>(
+    "https://localhost:7142/api/users"
+  );
+  return { data, isLoading, error };
+};
--- EventPlanner/src/main.tsx
+++ EventPlanner/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/HomePage.tsx";
+import App from "./pages/TestingPage.tsx";
 import "./index.css";
+import { SWRConfig } from "swr";
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <SWRConfig value={{ fetcher }}>
+      <App />
+    </SWRConfig>
   </React.StrictMode>
 );
