483ee8ea77bc41e81e870dd17586b8e9118b7aa4
Besarta Kuci <besartakuci@gmail.com>
2024-04-16 14:34:45 +0200

    main commit

--- EventPlanner/src/main.tsx
+++ EventPlanner/src/main.tsx
@@ -4,8 +4,16 @@ import App from "./pages/TestingPage.tsx";
 import "./index.css";
 import EventNavbar from './components/Navbar.tsx'
 import "./components/Navbar.css"
+import EventFooter from './components/Footer.tsx'
+import "./components/Footer.css"
+import ControlledCarousel from './components/Slider.tsx'
+import './components/Slider.css'
+
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <EventNavbar></EventNavbar>
+    <ControlledCarousel></ControlledCarousel>
+    <EventFooter></EventFooter>
   </React.StrictMode>
 );
