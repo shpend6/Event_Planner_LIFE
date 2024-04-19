b1614c02326f565ae3083e29ae861b29e1ef177c
shpend6 <shpend.ismaili1@gmail.com>
2024-04-19 17:55:29 -0400

    CORS error fixed

--- EventPlanner/src/main.tsx
+++ EventPlanner/src/main.tsx
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom/client";
 // import App from "./pages/TestingPage.tsx";
 import "./index.css";
 import { SWRConfig } from "swr";
-import  RegisterForm from "./pages/RegisterPage.tsx";
+import RegisterForm from "./pages/RegisterPage.tsx";
+import LoginForm from "./pages/LoginPage.tsx";
 // import LoginForm from "./pages/LoginPage.tsx";
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SWRConfig value={{ fetcher }}>
-<RegisterForm/>
-{/* <LoginForm/> */}
+      <RegisterForm />
+      <LoginForm />
     </SWRConfig>
   </React.StrictMode>
 );
