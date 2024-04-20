a7496a58b7a506372f1d7b82efefcb7a58bf9756
Besarta Kuci <besartakuci@gmail.com>
2024-04-20 13:34:53 +0200

    filter categories test

---
+++ EventPlanner/src/components/FilterTickets.tsx
@@ -0,0 +1,19 @@
+/*import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import './FilterTickets.css'
+function Filter() {
+  return (
+    <div className='select-div'>
+        <Form.Select aria-label="Floating label select example" className='selectBar'>
+        <option value="4">Select category</option>
+        <option value="1">Music</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </Form.Select>
+    </div>
+      
+   
+  );
+}
+
+export default Filter;*/
---
+++ EventPlanner/src/components/Slider.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import "./Slider.css"
+
+function ControlledCarousel() {
+  const [index, setIndex] = useState<number>(0); // Specify the type as number
+
+  const handleSelect = (selectedIndex: number) => { // Specify the type of selectedIndex
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img className='sliderimg' src="https://www.rave-travel.com/images/event/sanfest-1903x519-min.jpg" alt="" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='sliderimg' src="https://www.rave-travel.com/images/event/1_BK.jpg" alt="" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='sliderimg' src="https://www.rave-travel.com/images/event/EventImage%20(1).png" alt="" />
+      </Carousel.Item>
+      
+    </Carousel>
+  );
+}
+
+export default ControlledCarousel;
