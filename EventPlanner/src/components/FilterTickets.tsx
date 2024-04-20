3290903e44e9684ff95e19bdea49bbdf77265788
Besarta Kuci <besartakuci@gmail.com>
2024-04-20 15:48:30 +0200

    home events (20)

--- EventPlanner/src/components/FilterTickets.tsx
+++ EventPlanner/src/components/FilterTickets.tsx
@@ -1,43 +1,18 @@
-import Dropdown from 'react-bootstrap/Dropdown';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 import './FilterTickets.css'
 
-function FilterTickets(){
-    return(
-        <div className='filter_tickets'>
-            <h2>Explore events in</h2>
-            <hr /> 
-            <div className="block">
-            <div className='dropdown'>
-                <div className="time">
-                <Dropdown>
-                 <Dropdown.Toggle variant="light" id="dropdown-basic">
-                       Time 
-                 </Dropdown.Toggle>
-
-                     <Dropdown.Menu>
-                     <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                     <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                     </Dropdown.Menu>
-                     </Dropdown>
-                </div>
-                <div className="time">
-                <Dropdown>
-                 <Dropdown.Toggle variant="light" id="dropdown-basic">
-                       Location
-                 </Dropdown.Toggle>
-
-                     <Dropdown.Menu>
-                     <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                     <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                     </Dropdown.Menu>
-                     </Dropdown>
-                </div>
-            </div>
-            </div>
-        </div>
-    );
+function FiterTickets() {
+  return (
+    <div>
+      <Form.Select aria-label="Floating label select example" className='filter-drop'>
+        <option >Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </Form.Select>
+    </div>
+  );
 }
 
-export default FilterTickets
+export default FiterTickets;
--- EventPlanner/src/components/Navbar.tsx
+++ EventPlanner/src/components/Navbar.tsx
@@ -1,16 +1,18 @@
+import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom"; // Import Link
 
 function EventNavbar() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar style={{ backgroundColor: '#2d2d32' }} variant="dark" expand="lg">
       <Container>
         <Navbar.Brand href="#">Event Planner</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">About</Nav.Link>
-            <Nav.Link href="#">Contact</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/admin">About</Nav.Link> {/* Change to /admin for Admin page */}
+            <Nav.Link href="/contact">Contact</Nav.Link> {/* Assuming you have a /contact route */}
           </Nav>
         </Navbar.Collapse>
       </Container>
