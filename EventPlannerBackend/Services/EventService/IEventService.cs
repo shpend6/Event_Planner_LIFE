b09257386d98a4e032d7b2066787f07c7bb759e5
Donat Kusari <90781600+donzzi@users.noreply.github.com>
2024-04-20 12:03:28 +0200

    Added Organization and ImagePath to Event Model. When we create an event, we can also add an image. User doesn't have to update every field of the event, now only the changed fields will be updated.

--- EventPlannerBackend/Services/EventService/IEventService.cs
+++ EventPlannerBackend/Services/EventService/IEventService.cs
@@ -5,10 +5,12 @@ namespace EventPlanner.Server.Services.EventService;
 
 public interface IEventService
 {
+    Task<IEnumerable<GetEventsSummaryDto>> GetEventsSummaryAsync();
     Task<IEnumerable<Event>> GetAllEventsAsync();
     Task<Event> GetEventByIdAsync(int id);
     Task<Event> CreateEventAsync(Event newEvent);
-    Task UpdateEventAsync(int id, Event updatedEvent);
+    Task UpdateEventAsync(int id, UpdateEventDto updatedEvent);
     Task DeleteEventAsync(int id);
     Task<List<AttendeeDto>> GetAttendeesByEventIdAsync(int id);
+    Task<string> SaveImageAsync(IFormFile imageFile);
 }
