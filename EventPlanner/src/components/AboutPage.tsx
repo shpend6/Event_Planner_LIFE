5f2d7e33da9ab957888cfe7d09b298320441af7b
Besarta Kuci <besartakuci@gmail.com>
2024-04-20 16:19:05 +0200

    About/Routers

---
+++ EventPlanner/src/components/AboutPage.tsx
@@ -0,0 +1,8 @@
+function About(){
+    return(
+        <div>
+            <p>About</p>
+        </div>
+    )
+}
+export default About
--- EventPlanner/src/components/AppRoutes.tsx
+++ EventPlanner/src/components/AppRoutes.tsx
@@ -1,13 +1,13 @@
 //import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HomeComponent from '../pages/HomePage'; // Update the path as per your file structure
-import AdminComponent from '../pages/AdminPage'; // Update the path as per your file structure
+import AboutComponent from '../pages/AdminPage'; // Update the path as per your file structure
 
 function RRoutes() {
   return (
     <Routes>
     <Route path="/home" element={<HomeComponent />} />
-    <Route path="/admin/*" element={<AdminComponent />} />
+    <Route path="/admin/*" element={<AboutComponent />} />
     </Routes>
   );
 }
