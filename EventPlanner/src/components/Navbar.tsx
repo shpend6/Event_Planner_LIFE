17c4dd9348c4403bcaf1f3669f762d62519f5a81
vanesamulliqi <vanesamulliqi@gmail.com>
2024-04-22 14:31:50 +0200

    Fixing Navbar to HomePage

--- EventPlanner/src/components/Navbar.tsx
+++ EventPlanner/src/components/Navbar.tsx
@@ -1,21 +1,29 @@
-import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom"; // Import Link
+import { Navbar, Container, Nav, Form, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function EventNavbar() {
   return (
-    <Navbar style={{ backgroundColor: '#2d2d32' }} variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="#">Event Planner</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="ml-auto">
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="/">Event Planner</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About Us</Nav.Link> {/* Change to /about for About page */}
+            <Nav.Link href="/about">About Us</Nav.Link>
             <Nav.Link href="/contact">Contact Us</Nav.Link>
-            <Nav.Link href="/login">Log In</Nav.Link> 
-            <Nav.Link href="/signup"> Sign up</Nav.Link> 
           </Nav>
+          <Form className="d-flex">
+            <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+            <Button variant="outline-success" style={{background: 'linear-gradient(111.3deg, rgb(74, 105, 187) 9.6%, rgb(205, 77, 204) 93.6%)', color:'white'}}>Search</Button>
+            <div className="ms-4"></div>
+            <Nav.Link href="/login" className="d-flex align-items-center text-light">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-circle me-1" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+              </svg>
+            </Nav.Link>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- EventPlanner/src/main.tsx
+++ EventPlanner/src/main.tsx
@@ -4,9 +4,6 @@ import "./index.css";
 // import { SWRConfig } from "swr";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import BrowserRouter, Routes, and Route
 import "./index.css";
-import EventNavbar from './components/Navbar.tsx'
-import "./components/Navbar.css"
-import EventFooter from './components/Footer.tsx'
 import AboutComponent from './pages/AboutUs.tsx'; // Import your About component
 import HomeComponent from './pages/HomePage.tsx'
 import EventPage from './pages/EventPage.tsx'; // Import EventPage
@@ -15,21 +12,17 @@ import Login from "./pages/LoginPage.tsx";
 import Signup from './pages/Signup.tsx'
 
 
-// const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <EventNavbar/>
     <Router>
       <Routes>
         <Route path="/" element={<HomeComponent />} />
         <Route path="/about*" element={<AboutComponent />} />
-        <Route path="/contact" element={<Contact />} /> {/* Add route for EventPage */}
-        <Route path="/login" element={<Login/>} /> {/* Add route for EventPage */}
-        <Route path="/signup" element={<Signup />} /> {/* Add route for EventPage */}
-        <Route path="/eventdetails" element={<EventPage />} /> {/* Add route for EventPage */}
+        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/login" element={<Login/>} /> {/* Add route for LoginIn*/}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/eventdetails" element={<EventPage />} /> 
       </Routes>
-      <EventFooter />
     </Router>
   </React.StrictMode>
 );
