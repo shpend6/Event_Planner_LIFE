107275ed374da6853f0124f27885788753f8951f
shpend6 <shpend.ismaili1@gmail.com>
2024-04-24 15:59:11 -0400

    Get event by category fixed

--- EventPlannerBackend/Controllers/EventController.cs
+++ EventPlannerBackend/Controllers/EventController.cs
@@ -50,7 +50,7 @@ public class EventController : ControllerBase
         return Ok(eventItem);
     }
 
-    [HttpGet("{categoryName}")]
+    [HttpGet("get-events-by-category/{categoryName}")]
     public async Task<IActionResult> GetEventsByCategory(string categoryName)
     {
         var eventsByCategory = await _eventService.GetEventsByCategoryAsync(categoryName);
