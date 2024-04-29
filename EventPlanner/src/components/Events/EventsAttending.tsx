26db58468e26ebc8fce615a66384599517ee2655
Besarta Kuci <besartakuci@gmail.com>
2024-04-29 13:47:46 +0200

    cleaner code

--- EventPlanner/src/components/Events/EventsAttending.tsx
+++ EventPlanner/src/components/Events/EventsAttending.tsx
@@ -66,29 +66,4 @@ function EventsAttending({ userId }: Props) {
 }
 
 export default EventsAttending;
-// function ConfirmationModal({
-//   show,
-//   handleClose,
-//   handleConfirm,
-// }: {
-//   show: boolean;
-//   handleClose: () => void;
-//   handleConfirm: () => void;
-// }) {
-//   return (
-//     <Modal show={show} onHide={handleClose}>
-//       <Modal.Header closeButton>
-//         <Modal.Title>Confirm Action</Modal.Title>
-//       </Modal.Header>
-//       <Modal.Body>Are you sure you want to cancel this event?</Modal.Body>
-//       <Modal.Footer>
-//         <Button variant="secondary" onClick={handleClose}>
-//           Cancel
-//         </Button>
-//         <Button variant="primary" onClick={handleConfirm}>
-//           Confirm
-//         </Button>
-//       </Modal.Footer>
-//     </Modal>
-//   );
-// }
+
--- EventPlanner/src/main.tsx
+++ EventPlanner/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./index.css";
 import { SWRConfig } from "swr";
 import AboutComponent from "./pages/AboutUs.tsx";
 import HomeComponent from "./pages/HomePage.tsx";
